721db5667ef2a3ecba20c6d5a2267703e826e67b
annanas-dev <anna.nasidletskaia@yandex.ru>
2025-11-17 15:54:40 +0300

    Создана функция запуска программы + вспомогательные функции

---
+++ jacobi_solver_test.cpp
@@ -0,0 +1,93 @@
+// #include <gtest/gtest.h>
+#include <sstream> // чтобы перенаправить вывод std::cout в строку
+#include <string>
+#include <iostream>
+#include <vector>
+#include <omp.h>
+
+// Подключаем main.cpp, но с помощью макроса переименовываем его в jacobi_original_main,
+// чтобы он не конфликтовал с main в текущем файле с Google Test, 
+// т.е. перед компиляцией все слова main будут заменены на jacobi_original_main
+#define main jacobi_original_main
+#include "main.cpp"
+#undef main // отменяем этот макрос
+
+struct JacobiRunResult {
+  int ret_code{}; // return 0, другое значение - ошибка
+  int n{}; 
+  int threads{}; // кол-во потоков OpenMP, кот-ые использовались программлй
+  int iter{};
+  double residual{}; // относительная невязка
+  double time{}; // время работы основного алгоритма (итерации метода Якоби + вычисление невязки), в секундах
+};
+
+// Вспомогательная функция, кот-ая отвечает за то,
+// что мы можем вытащить из результата нужное нам значение по ключу (для int)
+// на входе ф-ия получает сам результат и тот ключ, значение кот-го нас интересует
+// например, ParseIntField(line, "iter=") и получим 100 (например)
+// static - ф-ия видна только внутри текущего файла
+static int ParseIntField(const std::string& line, const std::string& key) {
+  auto pos = line.find(key);
+  if (pos == std::string::npos) {
+    throw std::runtime_error("Key not found: " + key);
+  }
+  pos += key.size();
+  auto end = line.find_first_of(" \t\r\n", pos);
+  std::string value = line.substr(pos, end - pos);
+  return std::stoi(value);
+}
+
+// то же, что и выше, но для double
+static double ParseDoubleField(const std::string& line, const std::string& key) {
+  auto pos = line.find(key);
+  if (pos == std::string::npos) {
+    throw std::runtime_error("Key not found: " + key);
+  }
+  pos += key.size();
+  auto end = line.find_first_of(" \t\r\n", pos);
+  std::string value = line.substr(pos, end - pos);
+  return std::stod(value);
+}
+
+// Симуляция запуска программы с командной строки с заданными аргументами
+// она сбирает массив из этих аргументов, будто бы мы ввели его в командную строку
+// tol - точность, seed - значение для генератора
+static JacobiRunResult RunJacobi(int n, int max_iter, double tol, unsigned seed) {
+  std::vector<std::string> args;
+  args.emplace_back("prog"); // не важно, по соглашению argv[0] всегда чем-то заполнен
+  args.emplace_back(std::to_string(n)); // argv[1]
+  args.emplace_back(std::to_string(max_iter)); // argv[2]
+  args.emplace_back(std::to_string(tol)); // argv[3]
+  args.emplace_back(std::to_string(seed)); // argv[4]
+
+
+  std::vector<char*> argv; // вектор указателей на строки (точнее на первый символ каждой строк)
+  argv.reserve(args.size());
+  for (auto& s : args) {
+    argv.push_back(s.data()); // char**, указатель на первый элемент
+  }
+
+  // перенаправляем cout в строковый поток
+  std::ostringstream oss; // класс из <sstream>, поток, который пишет в строку, 
+  // т.е. вместо выода в консоль, вывод копится внутри oss в памяти
+  // мы записываем все в буфер:
+  std::streambuf* old_buf = std::cout.rdbuf(oss.rdbuf()); // сохраняем старый буфер в old_buf, это буфер консоли
+  // при вызове main, cout теперь «подключен» к oss и сё складывается в строковый буфер
+
+  int ret = jacobi_original_main(static_cast<int>(argv.size()), argv.data());
+
+  std::cout.rdbuf(old_buf); // возвращаемся к буферу консоли
+
+  std::string out = oss.str(); // сохраниям в переменную данный из строквого буфера
+
+  JacobiRunResult res;
+  res.ret_code = ret;
+  res.n = ParseIntField(out, "n=");
+  res.threads = ParseIntField(out, "threads=");
+  res.iter = ParseIntField(out, "iter=");
+  res.residual = ParseDoubleField(out, "residual=");
+  res.time = ParseDoubleField(out, "time=");
+
+  return res;
+}
+
